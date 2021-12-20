f1d45d16adac86f74384b6b8a4c99df66eecb8b8
Ky3bK4 <k.kyzmin321@gmail.com>
2021-12-20 12:56:21 +0200

    commit

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
+  const baseUrl = 'https://my-json-server.typicode.com/Ky3bK4/json-server';
   const filterCategory = category === null ? '' : `category=${category}`;
 
   dispatch(setLoaded(false));
+
   axios
-    .get(`/pizzas?${filterCategory}&_sort=${sortBy}`)
+    .get(`${baseUrl}/pizzas?${filterCategory}&_sort=${sortBy}`)
     .then(({ data }) => {
       dispatch(setPizzas(data));
     });
